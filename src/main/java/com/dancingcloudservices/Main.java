33cc4e5526ab693f3b7f6247b4dd31c3dd76b5e5
simon <simon@dancingcloudservices.com>
2024-01-08 12:33:34 -0700

    Initial

---
+++ src/main/java/com/dancingcloudservices/Main.java
@@ -0,0 +1,8 @@
+package com.dancingcloudservices;
+
+sealed interface X permits Main {}
+public final class Main implements X {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 17 world!");
+    }
+}
